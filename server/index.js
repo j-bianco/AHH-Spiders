744be93a40f704292c33b5d099ab12af0405ca0e
Anthony Bianco <anthony.bianco3@gmail.com>
2018-06-10 12:46:03 -0700

    [FEAT] initialize the express app

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || null;
+
+app.use(express.static(path.join(__dirname, '../app/public')));
+
+app.listen(port, host, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log(`listening on ${port}`);
+  }
+});
