923a093627c574f4913d39e0336839506b769d77
John Bianco <jfbianco6756@gmail.com>
2018-06-19 21:03:55 -0700

    new branch

---
+++ app/public/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("AHHSpidersApp", ["ui.router", "ngRoute"])
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/home")
+
+  $stateProvider
+    .state("home", {
+      url: "/home",
+      templateUrl: "/views/home.html",
+      controller: "homeController"
+    })
+})
